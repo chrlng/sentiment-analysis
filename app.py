a4318f5d14c0daef7bc5c3fc5de15b94c2bda4db
Christine Nguyen <christine_nguyenl@berkeley.edu>
2020-07-06 01:23:45 -0700

    yay! backend works

--- app.py
+++ app.py
@@ -1,27 +1,54 @@
 from flask import Flask, redirect, url_for, render_template, request, flash 
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
 
 app = Flask(__name__)
 app.secret_key = "hello"
 
+# function that returns a dictionary of scores
+def dictionary(sentence): 
+    #instantiate sentinment object
+    sent_obj = SentimentIntensityAnalyzer() 
+    
+    # dict has 'neg', 'pos', 'neu', and 'compound'
+    return sent_obj.polarity_scores(sentence) 
+
+    # if compound is >= 0.05, then it's positive 
+    # if compound is <= -0.05, then it's negative
+    # else it's neutral
+
+# takes in a compound score and outputs a string of positive, negative, or neutral
+def sentiment(score):
+    if score >= 0.05:
+        return "positive xD"
+    elif score <= -0.05:
+        return "negative poo poo"
+    return "neutral"
+
 
 #home page
-@app.route("/")
+@app.route("/", methods=["POST"])
 def home():
-    return render_template("index.html")
+    #use post to get the sentence in the form 
+    if request.method == "POST":
+        # get the data that was put into the input box 
+        data = request.form["data"]
+        if data:
+            score = dictionary(data)["compound"]
+            sent = sentiment(score)
+            return render_template("index.html", sent=sent, show_modal=True)
+        return render_template("index.html", show_modal=False)
+    #use modal to show the results 
+    return render_template("index.html", show_modal=False)
 
 #results page 
-@app.route("/results", methods=["POST"])
-def results():
-    if request.method == "POST": 
-        data = request.form["data"]
-        sentiment = classify(data)
-        return render_template("results.html")
-    else:
-        return render_template("index.html")
-
-# handle the classification
-def classify(data):
-    ...
+# @app.route("/results", methods=["POST"])
+# def results():
+#     if request.method == "POST": 
+#         data = request.form["data"]
+#         sentiment = classify(data)
+#         return render_template("results.html")
+#     else:
+#         return render_template("index.html")
 
 #run the app 
 if __name__ == "__main__":
